eeffbb3c484860701a841cdfe6957091d6e2377c
Dileepa Madusanka <sanka.dmg@gmail.com>
2023-06-12 18:59:57 +0530

    Set Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -29,4 +29,13 @@ app.get("/json", (req, res, next) => {
   }
 });
 
+const middleware = (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+};
+
+app.get("/now", middleware, (req, res) => {
+  res.send({ time: req.time });
+});
+
 module.exports = app;
