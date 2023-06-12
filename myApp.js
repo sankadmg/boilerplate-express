25f1f707919abf52a1211fb48ed8232ea70bf494
Dileepa Madusanka <sanka.dmg@gmail.com>
2023-06-12 18:48:04 +0530

    create logger middlware

--- myApp.js
+++ myApp.js
@@ -5,11 +5,19 @@ const app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
+// app.use(function middleware(req, res, next) {
+//   var string = req.method + " " + req.path + " - " + req.ip;
+//   console.log(string);
+//   next();
+// });
+
 app.get("/", function (req, res) {
   res.sendFile(path.resolve(__dirname, "/views/index.html"));
 });
 
-app.get("/json", (req, res) => {
+app.get("/json", (req, res, next) => {
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
   if (process.env.MESSAGE_STYLE === "uppercase") {
     res.json({
       message: "Hello json".toUpperCase(),
