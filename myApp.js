7dd0b9f862ed686e4300ca887194a705d35f6a2f
Dileepa Madusanka <sanka.dmg@gmail.com>
2023-06-12 17:07:30 +0530

    changes to required format

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+  res.sendFile(path.resolve(__dirname, "/views/index.html"));
 });
 
 module.exports = app;
