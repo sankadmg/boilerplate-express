793b2ade07e01f693d245ae7783550f4465e2d23
Dileepa Madusanka <sanka.dmg@gmail.com>
2023-06-12 17:26:11 +0530

    set request /json route

--- myApp.js
+++ myApp.js
@@ -8,4 +8,10 @@ app.get("/", function (req, res) {
   res.sendFile(path.resolve(__dirname, "/views/index.html"));
 });
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
+});
+
 module.exports = app;
