a4aad51cfb81dfe3558e50332e7fdc16c0a79b06
Dileepa Madusanka <sanka.dmg@gmail.com>
2023-06-12 11:36:39 +0530

    Serve the /views/index.html file with get('/')

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let app = express();
 app.get("/", function (req, res) {
-  res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 module.exports = app;
