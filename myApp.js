33fbfe201de0ce18123be4f140bc7d2332734fe6
Dileepa Madusanka <sanka.dmg@gmail.com>
2023-06-13 15:18:51 +0530

    update myApp.js file with uncomment

--- myApp.js
+++ myApp.js
@@ -1,13 +1,13 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const path = require("path");
 var bodyParser = require("body-parser");
 require("dotenv").config();
 
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
