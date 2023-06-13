1711cf58578b94fdeb205bbeda99a72377ef4028
Dileepa Madusanka <sanka.dmg@gmail.com>
2023-06-13 18:27:50 +0530

    updated

--- myApp.js
+++ myApp.js
@@ -4,11 +4,8 @@ const path = require("path");
 var bodyParser = require("body-parser");
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+mongoose.connect(process.env.MONGO_URI);
+console.log(con);
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
