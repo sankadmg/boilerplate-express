6ca0a10de89315f57f1ee91b723ad029d43a20c0
Dileepa Madusanka <sanka.dmg@gmail.com>
2023-06-13 07:15:03 +0530

    Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 var bodyParser = require("body-parser");
 require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
